a7c19212c1fc2847375665ed5758633faaa22bf5
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2024-08-28 22:01:48 +0300

    add homepage

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: 'https://github.com/OlegFurculita1408/goit-typescript-hw-02.git/',
-})
