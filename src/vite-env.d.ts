424c67105a06027c9118f9f564035cede9052fc2
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2024-08-28 21:56:32 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: 'https://github.com/OlegFurculita1408/goit-typescript-hw-02.git/',
+})
